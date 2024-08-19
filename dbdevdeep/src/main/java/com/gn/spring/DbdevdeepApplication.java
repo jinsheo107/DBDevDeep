6445787f6c9e8cdeefea2ddd093b70294d01a265
bo-gyung <hello91390@gmail.com>
2024-08-19 17:43:40 +0900

    [고보경] 레포지토리 생성, 깃이그노 설정

---
+++ dbdevdeep/src/main/java/com/gn/spring/DbdevdeepApplication.java
@@ -0,0 +1,13 @@
+package com.gn.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbdevdeepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbdevdeepApplication.class, args);
+	}
+
+}
