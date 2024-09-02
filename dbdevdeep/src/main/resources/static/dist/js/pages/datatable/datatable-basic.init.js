bf70cc255606df44f8e3b2bbba8378dad5a131c8
jinsheo107 <jinsheo107@gmail.com>
2024-09-02 17:48:50 +0900

    [진서형] paginate css 설정

--- dbdevdeep/src/main/resources/static/dist/js/pages/datatable/datatable-basic.init.js
+++ dbdevdeep/src/main/resources/static/dist/js/pages/datatable/datatable-basic.init.js
@@ -34,3 +34,18 @@ $('#multi_col_order').DataTable({
         orderData: [4, 0]
     }]
 });
+
+
+/******************************************
+ * 			Custom Table
+ * ****************************************/
+$('#address_book').DataTable({
+	info: false,
+	"sDom": '<"row view-filter"<"col-sm-12"<"pull-left"l><"pull-right"f><"clearfix">>>t<"row view-pager"<"col-sm-12"<"text-center"ip>>>',
+	language: {
+		paginate: {
+			previous: '이전',
+			next: '이후'
+		}
+	}
+});
