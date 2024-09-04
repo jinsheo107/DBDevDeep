54ab26ec03fba1dbddf11801e8b01a0853718c01
pbkan <pbkan4@gmail.com>
2024-09-04 13:39:54 +0900

    [박병준]맵핑 설정

--- dbdevdeep/src/main/java/com/dbdevdeep/approve/controller/ApproveViewController.java
+++ dbdevdeep/src/main/java/com/dbdevdeep/approve/controller/ApproveViewController.java
@@ -1,5 +1,18 @@
 package com.dbdevdeep.approve.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class ApproveViewController {
 
+	@GetMapping("/approve")
+	public String selectApproveList() {
+		return "approve/approList";
+	}
+	
+	@GetMapping("/approCreate")
+    public String showApproCreatePage() {
+        return "approve/approCreate";
+	}
 }
