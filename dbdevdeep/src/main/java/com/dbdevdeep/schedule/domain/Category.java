923ff8362744e4e80fa45cfe2da46d6a98f4300f
Park-somi <somp8320@gmail.com>
2024-09-05 11:45:22 +0900

    [박소미] 범주 domain

---
+++ dbdevdeep/src/main/java/com/dbdevdeep/schedule/domain/Category.java
@@ -0,0 +1,46 @@
+package com.dbdevdeep.schedule.domain;
+
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "category")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Builder
+public class Category {
+	
+	@Id
+	@Column(name = "category_no")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long categoryNo;
+	
+	@Column(name = "category_type")
+	private String categoryType;
+	
+	@Column(name = "category_name")
+	private String categoryName;
+	
+	@Column(name = "category_color")
+	private String categoryColor;
+	
+	@Column(name = "is_default")
+	private String isDefault;
+	
+	@OneToMany(mappedBy = "category")
+	private List<Schedule> schedules;
+	
+}
