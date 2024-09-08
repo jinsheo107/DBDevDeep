edad7488b55ce2b59195292cc1d3c09eb54282e2
bogyung <hello91390@gmail.com>
2024-09-09 00:12:16 +0900

    [고보경] 공지사항 게시판 CRUD 기능 구현 완료

--- dbdevdeep/src/main/java/com/dbdevdeep/notice/controller/NoticeApiController.java
+++ dbdevdeep/src/main/java/com/dbdevdeep/notice/controller/NoticeApiController.java
@@ -5,6 +5,8 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -16,7 +18,6 @@ import com.dbdevdeep.notice.service.NoticeService;
 public class NoticeApiController {
 	
 	private final NoticeService noticeService;
-//	private final FileService fileSeivice;
 	
 	@Autowired
 	public NoticeApiController(NoticeService noticeService) {
@@ -39,6 +40,35 @@ public class NoticeApiController {
 		return resultMap;
 	}
 	
+	// 공지사항 게시글 수정
+	@ResponseBody
+	@PostMapping("/notice/edit/{notice_no}")
+	public Map<String,String> updateNotice(@RequestBody NoticeDto dto){
+		Map<String,String> resultMap = new HashMap<String,String>();
+		resultMap.put("res_code", "404");
+		resultMap.put("res_msg", "게시글 작성 중 오류가 발생하였습니다.");
+		
+		if(noticeService.updateNotice(dto)>0) {
+			resultMap.put("res_code", "200");
+			resultMap.put("res_msg", "게시글 작성에 성공하였습니다.");
+		}
+		
+		return resultMap;
+	}
 	
+	// 공지사항 게시글 삭제
+	@ResponseBody
+	@DeleteMapping("/notice/{notice_no}")
+	public Map<String,String> deleteNotice(@PathVariable("notice_no") Long notice_no){
+		Map<String,String> map = new HashMap<String, String>();
+		map.put("res_code", "404");
+		map.put("res_msg", "게시글 삭제 중 오류가 발생했습니다.");
+		
+		if(noticeService.deleteNotice(notice_no) > 0) {
+			map.put("res_code", "200");
+			map.put("res_msg", "정상적으로 게시글이 삭제되었습니다.");			
+		}
+		return map;
+	}
 	
 }
